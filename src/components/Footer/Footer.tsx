b3035d3f5d32d2a910dcecac5a35ee2b0acac52a
canocalir <can.ocalir@gmail.com>
2023-03-08 17:50:20 +0300

    Changed footer weight

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,7 +4,7 @@ import { FooterContainer } from "./Footer.styled";
 const Footer: FC = () => {
   return <FooterContainer>
     <p>Altıntepe mh. İstasyon Yolu sk. No 3/1 Maltepe İstanbul, 34840 Türkiye</p>
-    © 2023 All Rights Reserved. Created by MicroFire
+    <strong>© 2023 All Rights Reserved. Created by MicroFire</strong>
   </FooterContainer>;
 };
 
